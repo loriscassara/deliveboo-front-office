a2b91353172cbb4b9b76b703c606078edfd3dc75
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-03-07 18:45:47 +0100

    test

--- src/debug.js
+++
@@ -1,8 +0,0 @@
-//Attenzione: accrocchio di livello leggendario, per soli scopi didattici
-
-export default function register(component) {
-    if (!window.vue[component.$options.name]) {
-        window.vue[component.$options.name] = [];
-    }
-    window.vue[component.$options.name].push(component);
-}
