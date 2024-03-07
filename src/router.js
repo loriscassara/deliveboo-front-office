6c43a7f587d955574a05baf42c170f4305f4d1cd
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-03-07 19:40:16 +0100

    Aggiungi nuovi file al repository

---
+++ src/router.js
@@ -0,0 +1,49 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppHome from "./pages/AppHome.vue";
+import EventList from "./pages/EventList.vue";
+import EventDetail from "./pages/EventDetail.vue";
+import ProductsList from "./pages/ProductsList.vue";
+import ProductSearch from "./pages/ProductSearch.vue";
+import ProductsFiltered from "./pages/ProductsFiltered.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: AppHome,
+      props: true
+    },
+    {
+      path: "/",
+      name: "Restaurants",
+      component: AppHome,
+      props: true
+    },
+    {
+      path: "/restaurants/:id",
+      name: "Restaurant-detail",
+      component: EventDetail,
+      props: true,
+    },
+    {
+      path: "/products",
+      name: "Products",
+      component: ProductsList,
+    },
+    {
+      path: "/search",
+      name: "ProductSearch",
+      component: ProductSearch,
+    },
+    {
+      path: "/update",
+      name: "ProductsFiltered",
+      component: ProductsFiltered,
+    },
+  ],
+});
+
+export { router };
