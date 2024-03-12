8c7830f06b38b2509f8a7512d296129473a86cef
Loris Cassarà <cassaralavoro@gmail.com>
2024-03-12 02:38:43 +0100

    style: added style

--- src/pages/AppSuccessful.vue
+++ src/pages/AppSuccessful.vue
@@ -12,12 +12,39 @@ export default {
 </script>
 
 <template>
-        <h1>Grazie per il tuo ordine</h1>
+        <div class="d-flex justify-content-center">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+        </div>
+        <h1 class="text-center pt-5 text-uppercase text-success fw-bold">Grazie per il tuo ordine!</h1>
+        <div class="d-flex justify-content-center">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+            <img class="rotate m-5" src="https://static.vecteezy.com/system/resources/previews/024/280/420/original/hot-and-fresh-tasty-delicious-grilled-hamburger-ai-generated-png.png" alt="">
+        </div>
         <router-link :to="{ name: 'Restaurants' }" class="btn btn-outline-dark w-25 m-auto d-flex justify-content-center mt-5 mb-5">
     <span>Torna alla lista ristoranti</span>
   </router-link>
      
 </template>
 
+<style>
+.rotate {
+    width: 300px;
+    animation: rotation 4s infinite linear;
+}
 
-
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+</style>
